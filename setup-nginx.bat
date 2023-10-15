ba1c9f6514874320aab93f7212a5d70c13a4fbc2
LazyCoder-1506 <pranayan1506@gmail.com>
2023-10-16 02:02:04 +0530

    added default nginx path and automatic project name extraction

--- setup-nginx.bat
+++ setup-nginx.bat
@@ -1,22 +1,30 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Get the root folder name
+for %%I in ("%CD%") do set "rootFolder=%%~nxI"
+
 REM Prompt user for Nginx installation path
-set /p "nginxPath=Enter Nginx installation path: "
+set /p "nginxPath=Enter Nginx installation path (or press Enter for default path C:\nginx-1.25.2): "
+
+REM Set default path if user input is empty
+if "!nginxPath!"=="" (
+    set "nginxPath=C:\nginx-1.25.2"
+)
 
 REM Check if the provided path exists
-if not exist "%nginxPath%" (
+if not exist "!nginxPath!" (
     echo Invalid path. Nginx installation path does not exist.
     exit /b
 )
 
-REM Check if dist/angular-frontend-framework folder exists
-if not exist "dist\angular-frontend-framework\" (
-    echo Source folder not found: dist\angular-frontend-framework\
+REM Check if folder exists
+if not exist "dist\!rootFolder!\" (
+    echo Source folder not found: dist\!rootFolder!\
     exit /b
 )
 
 REM Copy contents to Nginx/html folder
-xcopy /Y /S "dist\angular-frontend-framework\*" "%nginxPath%\html\"
+xcopy /Y /S "dist\!rootFolder!\*" "!nginxPath!\html\"
 
-echo Files copied successfully to %nginxPath%\html\
+echo Files copied successfully to !nginxPath!\html\
